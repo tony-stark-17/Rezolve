e7657424d719cfcaf75e4bd7b031ae3719fe29e2
Tony Stark <z.zedaniam@gmail.com>
2024-07-11 23:51:21 +0530

    feat: Security and additional functionalities

--- src/components/QuestionPage/QuestionPage.jsx
+++ src/components/QuestionPage/QuestionPage.jsx
@@ -1,7 +1,8 @@
-import { Input } from "@nextui-org/input";
-import { Button } from "@nextui-org/button";
+import { useLoaderData } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Input, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from "@nextui-org/react";
 import { Icon } from '@iconify/react';
-
+import toast, { Toaster } from 'react-hot-toast';
 import COET from '../../assets/coet.svg'
 import Logo from '../../assets/mulearn.svg'
 import Rezolve from '../../assets/rezolve.svg'
@@ -9,7 +10,51 @@ import Rezolve from '../../assets/rezolve.svg'
 
 import classes from './QuestionPage.module.css';
 
-const QuestionPage = () => {
+const QuestionPage = ({ index }) => {
+    const questions = useLoaderData();
+    const {isOpen, onOpen, onClose} = useDisclosure();
+    const [question, setQuestion] = useState('');
+    const [answer, setAnswer] = useState('');
+    const [answers, setAnswers] = useState();
+    const [link, setLink] = useState('');
+    const [img, setImg] = useState('');
+    useEffect(()=>{
+        setQuestion(questions[index].question);
+        setAnswers(questions[index].answers);
+        setLink(questions[index].link);
+        if(questions[index].imgLink){
+            setImg(questions[index].imgLink);
+        }
+    }, [])
+
+    const notify = (msg, type) =>{
+        toast(msg, {
+            position: 'bottom-right',
+            style:{
+                borderRadius: '10px',
+                background: type == 'success' ? '#3d9a3d' : '#ff4d4d',
+                color: 'white',
+                padding: '16px',
+                boxShadow: '0 4px 12px rgba(0,0,0,0.15)',
+                fontSize: '16px',
+                fontWeight: '500'
+            }
+        });
+    }
+
+    const SubmitAnswer = () =>{
+        if(answers.includes(answer.toLowerCase())){
+            onOpen();
+        }else{
+            notify('Wrong Answer!', 'error');
+        }
+    }
+    
+    const redirect = () =>{
+        window.open(link, '_blank');
+        onClose();
+    }
+
     return(
         <div className={`bg-slate-100 flex flex-col w-screen h-screen items-center py-5`}>
             <div className={`bg-slate-100 ${classes['header']}`}>
@@ -17,16 +62,44 @@ const QuestionPage = () => {
                     <img src={Rezolve} alt="Rezolve" className={classes['logo']}/>
                     <img src={Logo} alt="Logo" className={classes['mu-logo']}/>
             </div>
-            <div className={classes['question']}>
-                What is your name?
+            <div className="w-screen h-full flex flex-col justify-center items-center">
+                <div className={classes['question']}>
+                    {question}
+                </div>
+                {img != '' && <img src={img} alt="Question" className="max-w-[60%] rounded-3xl object-contain"/>}
+                <Input label="Answer" className="w-[50%] mt-10" size="lg" classNames={{
+                    inputWrapper: 'bg-slate-200 active:bg-slate-200 focus-within:!bg-slate-200',
+                }} endContent={
+                    <button className="mb-2" onClick={SubmitAnswer}>
+                        <Icon icon="akar-icons:arrow-right" className="text-[#fc6f09] hover:text-[#7048ff] hover:scale-125 transition-all ease-in-out" width="20" height="20"/>
+                    </button>
+                } onInput={(e) => setAnswer(e.target.value)} onKeyDown={(e) => (e.code == 'Enter') && SubmitAnswer()}/>
             </div>
-            <Input label="Answer" className="w-[50%] mt-20" size="lg" classNames={{
-                inputWrapper: 'bg-slate-200 active:bg-slate-200 focus-within:!bg-slate-200',
-            }} endContent={
-                <button className="mb-2">
-                    <Icon icon="akar-icons:arrow-right" className="text-[#fc6f09] hover:text-[#7048ff] hover:scale-125 transition-all ease-in-out" width="20" height="20"/>
-                </button>
-            }/>
+            <Modal isOpen={isOpen} backdrop="blur" isDismissable={false} onClose={onClose}>
+                <ModalContent>
+                {(onClose) => (
+                    <>
+                    <ModalHeader className="flex flex-col gap-1 text-xl text-[#fc6f09]">You have passed this level!</ModalHeader>
+                    <ModalBody>
+                        <p>
+                        Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit
+                        dolor adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis. 
+                        Velit duis sit officia eiusmod Lorem aliqua enim laboris do dolor eiusmod. 
+                        Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur 
+                        proident Lorem eiusmod et. Culpa deserunt nostrud ad veniam.
+                        </p>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="success" variant="flat" onPress={redirect}>
+                            <Icon icon="ic:baseline-whatsapp" width="1.4em" height="1.4em"/>
+                            Group Link
+                        </Button>
+                    </ModalFooter>
+                    </>
+                )}
+                </ModalContent>
+            </Modal>
+            <Toaster />
         </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {NextUIProvider} from '@nextui-org/react'
+import { NextUIProvider } from '@nextui-org/react'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import QuestionPage from './components/QuestionPage';
 
 import './index.css'
 
+const loadData = async () => {
+  const response = await fetch(
+    `https://api.npoint.io/0bec7c916682bda72d48`
+  );
+  const data = await response.json();
+  return data;
+}
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <QuestionPage/>,
+    element: <QuestionPage index={0}/>,
+    loader: loadData
+  },
+  {
+    path: "/2",
+    element: <QuestionPage index={1} />,
+    loader: loadData
+  },
+  {
+    path: "/3",
+    element: <QuestionPage index={2} />,
+    loader: loadData
+  },
+  {
+    path: "/4",
+    element: <QuestionPage index={5}/>,
+    loader: loadData
   },
 ]);
 
