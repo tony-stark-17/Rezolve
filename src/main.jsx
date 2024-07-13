e56d15780a6f85e81e96440795c919f54e90b3b6
Tony Stark <z.zedaniam@gmail.com>
2024-07-13 12:44:57 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -43,12 +43,12 @@ const router = createBrowserRouter([
   },
   {
     path: "/6",
-    element: <QuestionPage index={5} />,
+    element: <QuestionPage index={5} imgLink={qImg}/>,
     loader: loadData
   },
   {
     path: "/7",
-    element: <QuestionPage index={6} imgLink={qImg}/>,
+    element: <QuestionPage index={6}/>,
     loader: loadData
   },
 ]);
